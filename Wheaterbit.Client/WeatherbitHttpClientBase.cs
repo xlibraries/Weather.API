266a463469b9e08191b34e58829b0b950457bcd4
Xlib <aman9893089064@gmail.com>
2023-12-05 00:38:11 +0530

    protected the keys

---
+++ Wheaterbit.Client/WeatherbitHttpClientBase.cs
@@ -0,0 +1,30 @@
+﻿using FluentResults;
+using Wheaterbit.Client.Dtos;
+
+namespace Wheaterbit.Client
+{
+    internal class WeatherbitHttpClientBase
+    {
+
+        public async Task<Result<ForecastWeatherDto>> GetSixteenDayForecast(double latitude, double longitude, string cityName, CancellationToken cancellationToken)
+        {
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri($"{_options.Value.BaseUrl}/forecast/daily?lon={longitude}&lat={latitude}&city={cityName}"),
+                Headers =
+                {
+                    { XRapidAPIHostHeader, _options.Value.XRapidAPIHost },
+                    { XRapidAPIKeyHeader, _options.Value.XRapidAPIKey },
+                }
+            };
+
+            return await SendAsyncSave<ForecastWeatherDto>(request, cancellationToken);
+        }
+
+        private Task<Result<T>> SendAsyncSave<T>(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
